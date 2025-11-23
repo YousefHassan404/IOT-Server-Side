7b7d0ba7729779933de9d266352f1c89b3d62d64
Youssef Hassan <145775122+YousefHassan404@users.noreply.github.com>
2025-11-24 01:29:50 +0200

    Change server port from 4000 to 3000

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ io.on('connection', socket => {
 
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Backend listening on http://localhost:${PORT}`);
 });
